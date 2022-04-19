0f42413641b300f123b7a0bcdb093d16dbc102b1
Alana250 <103495599+Alana250@users.noreply.github.com>
2022-04-19 11:20:42 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
-input.onButtonPressed(Button.A, function () {
-    led.plot(0, 1)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        led.enable(true)
+        basic.clearScreen()
+        basic.showNumber(input.temperature())
+        basic.showString("DEGRES")
+        basic.showString("T=")
+    } else {
+        led.enable(true)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        led.enable(false)
+    }
 })
